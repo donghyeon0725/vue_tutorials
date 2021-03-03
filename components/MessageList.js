e75518fedcfc8e398cc71607810ecd2f2d51d90b
donghyeon0723 <edusolution0723@gmail.com>
2021-03-03 09:50:44 +0900

    2. 뷰 모듈화 추가 설명

--- components/MessageList.js
+++ components/MessageList.js
@@ -6,7 +6,7 @@ export default {
                     value="3" :itemCount="true"
                     @delete="deleteMessage(item)"></message-list-item>
                 </ol>`,
-    // 바인딩은 자식 모듈이 props를 통해 값을 가져가는 경우, 속성이 아닌,
+    // 바인딩은 기본적으로 태그안에 속성값을 두는 것이고, 자식 모듈이 props를 통해 값을 가져가서 바인딩하는 경우 해당 속성을 가지게 됨
     props: {
         //고립된 모듈의 스코프 특성상, 부모 컴포넌트의 값을 전달 받을 수 있도록 해놓은 장치
         items: {
